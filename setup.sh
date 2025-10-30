8c4699a72d5a23f494f248dbfbbb94a83f8c29f2
jlcoulter <104843199+jlcoulter@users.noreply.github.com>
2025-10-30 18:20:25 +1100

    Fix typo in hardware-configuration path

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 ln -s ~/flake/dots/* ~/.config/
 rm ~/flake/hosts/home/hardware-configuration.nix
 sudo nixos-generate-config
-sudo cp /etc/nixos/hardward-configuration.nix ~/flake/hosts/home/hardware-configuration.nix
+sudo cp /etc/nixos/hardware-configuration.nix ~/flake/hosts/home/hardware-configuration.nix
 sudo nixos-rebuild switch --flake ~/flake#default
