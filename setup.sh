2daf38a086602600fe7d75ba638e76ae896fde19
jlcoulter <104843199+jlcoulter@users.noreply.github.com>
2025-10-30 18:21:29 +1100

    Add reboot after NixOS configuration rebuild
    Add reboot command after NixOS rebuild

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ rm ~/flake/hosts/home/hardware-configuration.nix
 sudo nixos-generate-config
 sudo cp /etc/nixos/hardware-configuration.nix ~/flake/hosts/home/hardware-configuration.nix
 sudo nixos-rebuild switch --flake ~/flake#default
+reboot
