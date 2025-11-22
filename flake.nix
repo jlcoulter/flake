cea90eb963900120a5bdae607c4837bd1d17e14e
jlcoulter <coulter.l.jack@gmail.com>
2025-11-22 11:22:35 +1100

    init

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+    nvf.url = "github:notashelf/nvf";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nvf,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/home/configuration.nix
+        nvf.nixosModules.default
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
