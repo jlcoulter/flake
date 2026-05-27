4fc3a29d65abf8643a3309c26af6112f177f6071
jlcoulter <coulter.l.jack@gmail.com>
2026-05-27 13:00:16 +1000

    nvf.nix

--- hosts/macbook/default.nix
+++ hosts/macbook/default.nix
@@ -1,33 +1,34 @@
-{ pkgs, ... }: {
+{ pkgs, inputs, ... }: {
+  imports = [
+    ../../modules/nvim/nvf.nix
+  ];
+
   system.primaryUser = "jlcoulter";
 
   environment.systemPackages = with pkgs; [
     terminal-notifier
   ];
-  
+
   users.users.jlcoulter = {
     name = "jlcoulter";
     home = "/Users/jlcoulter";
-};
+  };
 
   nix.enable = false;
 
-  # macOS system configurations (dock, trackpad, finder, keyboard)
   system.defaults = {
     dock.autohide = true;
     finder.AppleShowAllExtensions = true;
     screencapture.location = "~/Downloads";
   };
 
-  # Optional: Manage Homebrew bundles using nix-darwin
   homebrew = {
     enable = true;
-    onActivation.cleanup = "zap"; # Removes apps not listed here
+    onActivation.cleanup = "zap";
     casks = [
       "kitty"
     ];
   };
 
-  # Necessary for making lookups comply with nix-darwin environment
-  system.stateVersion = 5; 
+  system.stateVersion = 5;
 }
